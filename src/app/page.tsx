3e210f4f02fbfcb553c9401fb32c35891570f488
Ink230 <justinquinn230@gmail.com>
2023-04-13 00:01:02 -0400

    final commit for the night

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <h1>Hello, Test!</h1>;
+  return (
+    <>
+      <h1>Hello, Test!</h1>
+      • lighthouse metrics, include the stuff i had to put in qws to this <br />
+      • review w/e else i had to do for qws or inkjq
+      <br />• start building things? SC MODS site?
+    </>
+  );
 }
