118c3ae9aa00236b0b8b875ddf8d7db49975fc4b
Ink230 <justinquinn230@gmail.com>
2023-04-14 00:38:04 -0400

    locale

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+    domains: [
+      {
+        domain: 'localhost',
+        defaultLocale: 'en',
+      },
+    ],
+    localeDetection: false,
+  },
 };
 
 module.exports = nextConfig;
